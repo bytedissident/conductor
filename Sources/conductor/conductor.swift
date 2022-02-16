843c4244b01e4ed0e987f1a35255a2b7c2248a2f
Derek Bronston <dbronston@me.com>
2022-02-16 11:48:03 -0500

    initial repo

---
+++ Sources/conductor/conductor.swift
@@ -0,0 +1,6 @@
+public struct conductor {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/conductorTests/conductorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import conductor
+
+final class conductorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(conductor().text, "Hello, World!")
+    }
+}
